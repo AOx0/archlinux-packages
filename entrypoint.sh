e0ce94b2db055c14f078581c343de2b58c905bac
Alejandro Osornio <50227494+AOx0@users.noreply.github.com>
2023-10-16 15:43:08 -0600

    Update hyprland PKGBUILD

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,7 +22,7 @@ rm ./gpg_key
 gpg --list-keys --keyid-format=long
 gpg --list-secret-keys --keyid-format=long
 
-for i in helix-git typst-git gitkraken-wayland; do
+for i in helix-git hyprland-git typst-git gitkraken-wayland; do
 	status=13
 	git submodule update --init $i
 	cd $i
