f0e0bffa2510266ffb6c2fa566408cd69c2308f8
Alejandro Osornio <aoxo.contact@gmail.com>
2024-06-20 20:52:02 -0600

    Re-add gitkraken-wayland

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,7 +22,7 @@ rm ./gpg_key
 gpg --list-keys --keyid-format=long
 gpg --list-secret-keys --keyid-format=long
 
-for i in typst-git helix-git; do
+for i in gitkraken-wayland typst-git helix-git; do
 	status=13
 	git submodule update --init $i
 	cd $i
